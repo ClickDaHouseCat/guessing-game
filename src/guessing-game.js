f4667f567b97263d0fd27e31df2154734024e864
Evgenii Dementev <63846200+ClickDaHouseCat@users.noreply.github.com>
2023-10-29 16:18:03 +0300

    feat: some complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.minRange = 0;
+      this.maxRange = 0;
+    }
 
     setRange(min, max) {
-
+      this.minRange = min;
+      this.maxRange = max;
     }
 
     guess() {
-
+      return Math.floor((this.minRange + this.maxRange) / 2);
     }
 
     lower() {
-
+      this.maxRange = this.guess();
     }
 
     greater() {
-
+      this.minRange = this.guess() + 1;
     }
-}
+  }
 
 module.exports = GuessingGame;
