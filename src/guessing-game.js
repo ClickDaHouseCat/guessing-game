4e90501bf7513bef9884c41226106b9f95f4d957
Evgenii Dementev <63846200+ClickDaHouseCat@users.noreply.github.com>
2023-10-29 23:08:52 +0300

    feat: some fix

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,33 +1,28 @@
 class GuessingGame {
     constructor() {
-      this.min = null;
-      this.max = null;
-      this.currentGuess = null;
+        this.min = 0;
+        this.max = 0;
+        this.currentGuess = 0;
     }
 
     setRange(min, max) {
-      this.min = min;
-      this.max = max;
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-        if (this.min >= this.max) {
-          this.currentGuess = this.min;
-        } else {
-          this.currentGuess = Math.round((this.min + this.max) / 2);
-        }
+        this.currentGuess = Math.round((this.min + this.max) / 2);
         return this.currentGuess;
-      }
+    }
+
     lower() {
-      // Обновляем верхнюю границу так, чтобы она была строго меньше текущего предполагаемого числа
-      this.max = this.currentGuess - 1;
+        this.max = this.currentGuess;
     }
 
     greater() {
-      // Обновляем нижнюю границу так, чтобы она была строго больше текущего предполагаемого числа
-      this.min = this.currentGuess + 1;
+        this.min = this.currentGuess;
     }
-  }
+}
 
 
   module.exports = GuessingGame;
